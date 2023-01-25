3332f6da1ad770423a44bc5eb1a78b8cbea76966
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2023-01-25 14:31:57 +0100

    add register route to controller

--- server/server.js
+++ server/server.js
@@ -4,12 +4,15 @@ require("./config/database").connect();
 const express = require("express")
 const app = express()
 
+
 const { API_PORT } = process.env;
 const port = API_PORT;
 
-app.get("/", (req, res) => {
-    res.status(200).json("Hello")
-    })
+const authController = require("./controller/authController")
+
+app.use(express.json());
+app.use('/auth', authController);
+
 
 app.listen(port, () => {
     console.log(`Application started at http://localhost:${port}`)
