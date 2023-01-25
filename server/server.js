782b88acf25d044aeb69e13d5c5e714344fa6b29
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2023-01-25 10:08:18 +0100

    add runnung server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
+
 const express = require("express")
 const app = express()
 
+
+const port = 3000
+
 app.get("/", (req, res) => {
+    res.status(200).json("Hello")
+    })
 
+app.listen(port, () => {
+    console.log(`Application started at http://localhost:${port}`)
 })
-
-const pokemonRouter = require('./routes/pokemons')
-
-app.use('/pokemons', pokemonRouter)
-
-app.listen(3000)
 
 
