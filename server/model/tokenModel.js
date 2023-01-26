694e68ec2db697f1633cb8e6ef7f275ebca0d280
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2023-01-26 11:52:44 +0100

    add model file for refreshtoken named token.js

---
+++ server/model/tokenModel.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const tokenSchema = new mongoose.Schema({
+  refreshToken: { type: String }
+
+});
+
+module.exports = mongoose.model("tokenr", tokenSchema);
